18698be8199fdb9d9f258bd88698772d43ddda4c
Tammo Ippen <tammo.ippen@posteo.de>
2021-02-01 12:31:53 +0100

    music

--- keyboards/nullbitsco/nibble/keymaps/tammoippen/keymap.c
+++ keyboards/nullbitsco/nibble/keymaps/tammoippen/keymap.c
@@ -28,7 +28,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BL] = LAYOUT_iso(
              KC_GRAVE,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_WWW_SEARCH,
     KC_MUTE, KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,          KC_DEL,
-    KC_ESC, KC_CAPS,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_NUHS, KC_ENT,  KC_NO,
+    KC_ESC,  KC_CAPS,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_NUHS, KC_ENT,  KC_NO,
     KC_NO,   KC_LSFT,   KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP,   KC_NO,
     KC_NO,   KC_LCTL,   KC_LGUI, KC_LALT,                            KC_SPC,                    MO(_FN), MO(_FN), KC_ALGR, KC_LEFT, KC_DOWN, KC_RGHT
   ),
@@ -37,7 +37,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
              _______,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_F13,      KC_SYSTEM_SLEEP,
     _______, _______, MO(_QMK), _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______, _______,              KC_SYSTEM_WAKE,
     KC_F16,  _______,  _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______, _______, KC_KP_ENTER, KC_F14,
-    _______, _______,  _______, RGB_MOD, RGB_TOG, _______, _______, _______, KC_MSTP, KC_MPLY, KC_MRWD,  KC_MFFD, _______, _______, KC_PGUP,     KC_F15,
+    _______, _______,  _______, RGB_MOD, RGB_TOG, _______, _______, _______, KC_MSTP, KC_MPLY, KC_MPRV,  KC_MNXT, _______, _______, KC_PGUP,     KC_F15,
     _______, _______,  _______, _______,                            _______,                   _______,  _______, _______, KC_HOME, KC_PGDN,     KC_END
   ),
 
